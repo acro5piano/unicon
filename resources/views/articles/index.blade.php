898e5d62f3db8b170d234195d98952a1aaf48868
gosho-kazuya <gosho-kazuya@919.jp>
2017-03-05 17:32:44 +0900

    add vue component

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,6 +1,9 @@
 <html>
     <body>
+        <example></example>
         @foreach ($articles as $article)
             <li>{{ $article->title }}</li>
         @endforeach
+    </body>
+    <script src="{{ mix('js/app.js') }}"></script>
 </html>
